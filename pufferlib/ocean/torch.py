277ee889a1aef4ddf96da87ba4a0126579343431
mx2000 <max.aguirre2424b@gmail.com>
2025-09-05 07:47:18 +0200

    fix METRA lstm

--- pufferlib/ocean/torch.py
+++ pufferlib/ocean/torch.py
@@ -1088,7 +1088,7 @@ class METRA(nn.Module):
             nn.GELU(),
             nn.Linear(hidden_size, hidden_size),
             nn.GELU(),
-            # nn.Linear(self.hidden_size, skill_dim),
+            nn.Linear(self.hidden_size, skill_dim),
         )
 
         self.log_lambda = nn.Parameter(torch.log(torch.tensor(30.0)))
@@ -1113,15 +1113,9 @@ class METRA(nn.Module):
 
         # Output FFN with spectral normalization
         self.phi_output = nn.Sequential(
-            nn.utils.parametrizations.spectral_norm(
-                nn.Linear(hidden_size, hidden_size),
-                n_power_iterations=1
-            ),
+            nn.Linear(hidden_size, hidden_size),
             nn.GELU(),
-            nn.utils.parametrizations.spectral_norm(
-                nn.Linear(hidden_size, skill_dim),
-                n_power_iterations=1
-            ),
+            nn.Linear(hidden_size, skill_dim),
         )
 
     @property 
@@ -1130,18 +1124,18 @@ class METRA(nn.Module):
         
     def phi_forward(self, observations, state=None):
 
-        # x = observations
-        # x_shape, space_shape = x.shape, self.obs_shape
-        # x_n, space_n = len(x_shape), len(space_shape)
-        # if x_n == space_n + 1:
-        #     B, TT = x_shape[0], 1
-        # elif x_n == space_n + 2:
-        #     B, TT = x_shape[:2]
-        # else:
-        #     raise ValueError('Invalid input tensor shape', x.shape)
-        # x = x.reshape(B*TT, *space_shape)
+        x = observations
+        x_shape, space_shape = x.shape, self.obs_shape
+        x_n, space_n = len(x_shape), len(space_shape)
+        if x_n == space_n + 1:
+            B, TT = x_shape[0], 1
+        elif x_n == space_n + 2:
+            B, TT = x_shape[:2]
+        else:
+            raise ValueError('Invalid input tensor shape', x.shape)
+        x = x.reshape(B*TT, *space_shape)
 
-        # return self.phi(x.float())
+        return self.phi(x.float())
 
         # LSTM stuff
         x = observations
